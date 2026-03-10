80a93c76062c1b15873d18462d28c9b676e80422
christianempire <christianempire@hotmail.com>
2026-03-10 16:37:48 -0500

    Reorganized project structure and setup build pipeline

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const { src, dest, series } = require("gulp");
+const { rm } = require("fs/promises");
+
+function clean() {
+  return rm("dist", { recursive: true, force: true });
+}
+
+function copy() {
+  return src("src/**/*").pipe(dest("dist"));
+}
+
+exports.build = series(clean, copy);
+exports.default = exports.build;
